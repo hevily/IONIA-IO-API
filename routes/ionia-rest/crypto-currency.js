4cf29ff8cb66626cc881f8a5c0189ec3ed11462a
yoojasung <arnold.yoo@findexchain.com>
2018-03-20 15:19:45 +0900

    chore: change res format to json

--- routes/ionia-rest/crypto-currency.js
+++ routes/ionia-rest/crypto-currency.js
@@ -4,7 +4,12 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const id = req.query.id;
-  res.send('Crypto Currency APIS  id : '+id);
+  const resObj = {
+    id: id,
+    uri: 'cryptoc',
+    method: 'get'
+  }
+  res.json(resObj);
 });
 
 router.post('/', function(req, res, next) {
