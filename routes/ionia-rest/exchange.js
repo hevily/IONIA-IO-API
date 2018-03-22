430089fc3a276d21102489ee4dcbd235d43bfdd9
David Kang <davidkang200@gmail.com>
2018-03-22 13:49:41 +0900

    remove ionia-rest folder

--- routes/ionia-rest/exchange.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const exchange = require('../../common/exchange/exchange');
-
-router.post('/', function(request, response, next) {
-  return exchange.exchangeHandler(request, response);
-});
-
-module.exports = router;
