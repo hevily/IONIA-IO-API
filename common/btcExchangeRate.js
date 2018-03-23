8458674642e5a6d71c3ea1f33a903d9cbb1a1a3e
changmyeong <ga970822@gmail.com>
2018-03-24 03:34:04 +0900

    delete express based code

--- common/btcExchangeRate.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-
-function getTicker(callback) {
-    request.get('https://blockchain.info/ticker', function(error, response, body) {
-        callback(JSON.parse(body));
-    });
-}
-
-exports.getTicker = getTicker;
--- common/coinMarketCap.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-
-function getTicker(callback) {
-    request.get('https://api.coinmarketcap.com/v1/ticker/', function(error, response, body) {
-        callback(JSON.parse(body));
-    });
-}
-
-exports.getTicker = getTicker;
