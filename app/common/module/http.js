1beab89f02b0e6298462315eadebffe98cab064b
changmyeong <ga970822@gmail.com>
2018-03-25 02:01:23 +0900

    fix http post method

--- app/common/module/http.js
+++ app/common/module/http.js
@@ -1,30 +1,37 @@
 const axios = require('axios');
+const querystring = require('querystring');
 
-async function requestHttp(url, method='GET', headers={}, params={}) {
-    const options = {
-        params: params,
-        headers: headers
+async function requestHttp(url, method='GET', headers={}, dataOrParams={}) {
+    headers['Connection'] = 'keep-alive';
+    headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    headers['Accept'] = '*/*';
+
+    const requestOptions = {
+        method: method,
+        url: url,
+        headers: headers,
     }
 
-    let responseBody = '';
+    if(method === 'GET') {
+        requestOptions['params'] = querystring.stringify(dataOrParams);
+    }
+    else if(method === 'POST') {
+        requestOptions['data'] = querystring.stringify(dataOrParams);
+    }
+    else {
+        throw 'HTTP Method ' + method + ' is Not found!';
+    }
+
+    let response;
 
     try {
-        if(method === 'GET') {
-            response = await axios.get(url, options);
-        }
-        else if(method === 'POST') {
-            response = await axios.post(url, options);
-        }
-        else {
-            throw method + ' is Not found!';
-        }
-
-        responseBody = response.data;
+        response = await axios(requestOptions);
+        response = response.data;
     } catch(error) {
-        console.log('http error');
+        console.log(error);
     }
 
-    return responseBody;
+    return response;
 }
 
 exports.requestHttp = requestHttp;
