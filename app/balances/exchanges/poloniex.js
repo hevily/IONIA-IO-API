8fcb9fd9fad7dc63af67b299b0c2c71f30a7c8cc
changmyeong <ga970822@gmail.com>
2018-03-25 03:30:50 +0900

    stringify object at sha512

--- app/balances/exchanges/poloniex.js
+++ app/balances/exchanges/poloniex.js
@@ -4,13 +4,13 @@ const url = 'https://poloniex.com/tradingApi';
 
 async function getBalances(data) {
     const requestBody = {
-        command: 'returnDepositAddresses',
+        command: 'returnBalances',
         nonce: new Date().getTime() * 100
     };
 
     const headers = {
         Key: data.poloniex.apiKey,
-        Sign: crypto.sha512(data.poloniex.secretKey, `command=${requestBody.command}&nonce=${requestBody.nonce}`)
+        Sign: crypto.sha512(data.poloniex.secretKey, requestBody)
     }
 
     const response = await http.request(url, 'POST', headers, requestBody);
--- app/common/module/crypto.js
+++ app/common/module/crypto.js
@@ -1,6 +1,11 @@
 const package_sha512 = require('sha512');
+const querystring = require('querystring');
 
 function sha512(key, data) {
+    if(typeof(data) === "object") {
+        data = querystring.stringify(data);
+    }
+
     const hasher = package_sha512.hmac(key);
     return hasher.finalize(data).toString('hex');
 }
