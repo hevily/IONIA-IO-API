e9e3be6f2eac8aec6fc237e9fd61e2863d6e933e
changmyeong <ga970822@gmail.com>
2018-03-25 20:43:10 +0900

    install and use js-base64

--- app/common/module/crypto.js
+++ app/common/module/crypto.js
@@ -1,5 +1,6 @@
 const package_sha512 = require('sha512');
 const package_sha256 = require('js-sha256').sha256;
+const package_base64 = require('js-base64').Base64;
 const querystring = require('querystring');
 
 function sha512_hmac(key, data) {
@@ -21,3 +22,4 @@ function sha256_hmac(key, data) {
 
 exports.sha512_hmac = sha512_hmac;
 exports.sha256_hmac = sha256_hmac;
+exports.base64 = package_base64;
