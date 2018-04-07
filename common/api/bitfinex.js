86b55dc7ca02a38270bd170cce30edb0bacb90d8
changmyeong <ga970822@gmail.com>
2018-04-07 17:38:54 +0900

    add getaddress of bitfinex

--- common/api/bitfinex.js
+++ common/api/bitfinex.js
@@ -3,13 +3,58 @@ const crypto = require('../modules/crypto')
 
 
 async function getbalances(data) {
-    const baseUrl = 'https://api.bitfinex.com'
-    const serviceUri = '/v1/balances'
-
+    const uri = '/v1/balances'
     const requestBody = {
         request: serviceUri,
         nonce: Date.now().toString()
     }
+
+    const balances = await requestToBitfinex(uri, data, requestBody)
+
+    const result = {}
+
+    if(response.message === undefined) {
+        const bitfinexObject = result['bitfinex'] = {}
+
+        for(let i = 0; i < tokens.length; i++) {
+            const token = tokens[i]
+
+            if(token.type === 'deposit' && token.name !== 'usd') {
+                bitfinexObject[token.currency] = {
+                    available: parseFloat(token.available),
+                    balance: parseFloat(token.amount),
+                    pending: 0
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+async function getaddress(data) {
+    const uri = '/v1/deposit/new'
+    const requestBody = {
+        request: uri,
+        nonce: Date.now().toString(),
+        method: changeSymbolToMethod(data.currency),
+        wallet_name: 'deposit'
+    }
+
+    const addresses = await requestToBitfinex(uri, data, requestBody)
+    
+    const result = {}
+
+    if(addresses.result === 'success') {
+        result['bitfinex'] = {}
+        result.bitfinex[data.currency] = addresses.address
+    }
+
+    return result
+}
+
+async function requestToBitfinex(uri, data, requestBody) {
+    const host = 'https://api.bitfinex.com'
 
     const payload = crypto.encode('base64', JSON.stringify(requestBody))
     const signature = crypto.hmac('sha384', data.bitfinex.secretKey, payload)
@@ -20,29 +65,24 @@ async function getbalances(data) {
         'X-BFX-SIGNATURE': signature
     }
 
-    const response = await http.request(baseUrl + serviceUri, 'POST', headers, requestBody)
-    console.log(response)
-    return response.message === undefined ? makeResult(response) : {}
+    const response = await http.request(host + uri, 'POST', headers, requestBody)
+
+    return response
 }
 
-function makeResult(tokens) {
-    const result = {}
-    const bitfinexObject = result['bitfinex'] = {}
-
-    for(let i = 0; i < tokens.length; i++) {
-        const token = tokens[i]
-
-        if(token.type === 'deposit' && token.name !== 'usd') {
-            bitfinexObject[token.currency] = {
-                available: parseFloat(token.available),
-                balance: parseFloat(token.amount),
-                pending: 0,
-                address: null
-            }
-        }
-    }
-
-    return result
+function changeSymbolToMethod(currency) {
+    return {
+        BTC: 'bitcoin',
+        LTC: 'litecoin',
+        ETH: 'ethereum',
+        ETC: 'ethereumc',
+        ZEC: 'zcash',
+        XMR: 'monero',
+        IOTA: 'iota',
+        BCH: 'bcash'
+        // tetheruso - ?
+    }[currency]
 }
 
 exports.getbalances = getbalances
+exports.getaddress = getaddress
