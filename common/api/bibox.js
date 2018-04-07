de7d6dbd7ed52144aafb23d7e5782ec7ec001886
changmyeong <ga970822@gmail.com>
2018-04-07 21:23:11 +0900

    add getaddress function of bibox

--- common/api/bibox.js
+++ common/api/bibox.js
@@ -4,7 +4,7 @@ const querystring = require('querystring')
 
 
 async function getbalances(data) {
-    const url = 'https://api.bibox.com/v1/transfer'
+    const uri = '/v1/transfer'
     const cmds = [{
         cmd: "transfer/assets",
         body: {
@@ -17,27 +17,62 @@ async function getbalances(data) {
         sign: crypto.hmac('md5', data.bibox.secretKey, JSON.stringify(cmds))
     }
 
-    const response = await http.request(url, 'POST', {}, requestBody)
+    const balances = await requestToBibox(uri, data, requestBody)
 
-    return response.error === undefined ? makeResult(response.result[0].result.assets_list) : {}
-}
-
-function makeResult(assetsList) {
     const result = {}
-    const biboxObject = result['bibox'] = {}
 
-    for(let i = 0 ;  i < assetsList.length ; i++) {
-        const coin = assetsList[i]
+    if(balances.error === undefined) {
+        const assetsList = balances.result[0].result.assets_list
+        const biboxObject = result['bibox'] = {}
 
-        biboxObject[coin.coin_symbol.toLowerCase()] = {
-            available: coin.balance,
-            balance: coin.balance,
-            pending: 0,
-            address: null
+        for(let i = 0 ;  i < assetsList.length ; i++) {
+            const coin = assetsList[i]
+
+            biboxObject[coin.coin_symbol.toLowerCase()] = {
+                available: coin.balance,
+                balance: coin.balance,
+                pending: 0
+            }
         }
     }
 
     return result
 }
 
+async function getaddress(data) {
+    const uri = '/v1/transfer'
+    const cmds = [{
+        cmd: "transfer/transferIn",
+        body: {
+            coin_symbol: data.currency
+        }
+    }]
+    const requestBody = {
+        cmds: JSON.stringify(cmds),
+        apikey: data.bibox.apiKey,
+        sign: crypto.hmac('md5', data.bibox.secretKey, JSON.stringify(cmds))
+    }
+
+    const addresses = await requestToBibox(uri, data, requestBody)
+
+    const result = {}
+
+    if(addresses.error === undefined) {
+        result['bibox'] = {}
+        result.bibox[data.currency] = addresses.result[0].result
+    }
+
+    return result
+}
+
+async function requestToBibox(uri, data, requestBody) {
+    const host = 'https://api.bibox.com'
+
+    const response = await http.request(host + uri, 'POST', {}, requestBody)
+    console.log('===', response)
+    return response
+}
+
+
 exports.getbalances = getbalances
+exports.getaddress = getaddress
