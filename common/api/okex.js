987b1959f989e9cd2bf62148f3949f0f491e2d51
changmyeong <ga970822@gmail.com>
2018-04-07 21:39:08 +0900

    refactor okex

--- common/api/okex.js
+++ common/api/okex.js
@@ -3,39 +3,44 @@ const crypto = require('../modules/crypto')
 
 
 async function getbalances(data) {
-    const url = 'https://www.okex.com/api/v1/userinfo.do'
+    const uri = '/api/v1/userinfo.do'
 
     const requestBody = {
         api_key: data.okex.apiKey
     }
-
-    const headers = {
-        'Content-type' : 'application/x-www-form-urlencoded',
-    }
-
     requestBody.sign = crypto.hash('md5', `api_key=${requestBody.api_key}&secret_key=${data.okex.secretKey}`).toUpperCase()
 
-    const response = await http.request(url, 'POST', headers, requestBody)
+    const response = await requestToOkex(uri, data, requestBody)
 
-    return response.result === true ? makeResult(response.info.funds) : {}
-}
-
-function makeResult(funds) {
     const result = {}
-    const okexObject = result['okex'] = {}
 
-    const availableCoins = funds.free
+    if(response.result === true) {
+        const okexObject = result['okex'] = {}
 
-    for(const coinName in availableCoins) {
-        okexObject[coinName] = {
-            available: parseFloat(availableCoins[coinName]),
-            balance: parseFloat(availableCoins[coinName]),
-            address: null,
-            pending: 0
+        const availableCoins = funds.free
+
+        for(const coinName in availableCoins) {
+            okexObject[coinName] = {
+                available: parseFloat(availableCoins[coinName]),
+                balance: parseFloat(availableCoins[coinName]),
+                pending: 0
+            }
         }
     }
 
     return result
 }
+
+async function requestToOkex(uri, data, requestBody) {
+    const host = 'https://www.okex.com'
+    const headers = {
+        'Content-type' : 'application/x-www-form-urlencoded',
+    }
+
+    const response = await http.request(host + uri, 'POST', headers, requestBody)
+
+    return response
+}
+
 
 exports.getbalances = getbalances
