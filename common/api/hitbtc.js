4fe0041e900e0d47f000b304502f9296eae70152
changmyeong <ga970822@gmail.com>
2018-04-07 20:59:28 +0900

    add getaddress function of hitbtc

--- common/api/hitbtc.js
+++ common/api/hitbtc.js
@@ -2,34 +2,54 @@ const http = require('../modules/http')
 
 
 async function getbalances(data) {
-    const url = 'https://api.hitbtc.com/api/2/account/balance'
+    const uri = '/api/2/account/balance'
+    const balances = await requestToHitbtc(uri, data, {})
 
-    const auth = {
-        username: data.hitbtc.apiKey,
-        password: data.hitbtc.secretKey
+    const result = {}
+
+    if(balances.error === undefined) { 
+        const hitbtcObject = result['hitbtc'] = {}
+
+        for(let i = 0; i < tokens.length; i++) {
+            const token = tokens[i]
+
+            hitbtcObject[token.currency] = {
+                available: parseFloat(token.available),
+                balance: parseFloat(token.available),
+                pending: 0
+            }
+        }
     }
-
-    const response = await http.request(url, 'GET', {}, {}, auth)
-
-    return makeResult(response)
+    
+    return result
 }
 
-function makeResult(tokens) {
+async function getaddress(data) {
+    const uri = `/api/2/account/crypto/address/${data.currency}`
+    const addresses = await requestToHitbtc(uri, data, {})
+
     const result = {}
-    const hitbtcObject = result['hitbtc'] = {}
-
-    for(let i = 0; i < tokens.length; i++) {
-        const token = tokens[i]
-
-        hitbtcObject[token.currency] = {
-            available: parseFloat(token.available),
-            balance: parseFloat(token.available),
-            pending: 0,
-            address: null
-        }
+    
+    if(addresses.error === undefined) {
+        result['hitbtc'] = {}
+        result.hitbtc[data.currency] = addresses.address
     }
 
     return result
 }
 
+async function requestToHitbtc(uri, data, requestBody) {
+    const host = 'https://api.hitbtc.com'
+    const auth = {
+        username: data.hitbtc.apiKey,
+        password: data.hitbtc.secretKey
+    }
+
+    const response = await http.request(host + uri, 'GET', {}, requestBody, auth)
+
+    return response
+}
+
+
 exports.getbalances = getbalances
+exports.getaddress = getaddress
