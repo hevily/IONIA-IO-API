c127c379fa89d2738c906b3f30b6dae1549cc05c
changmyeong <ga970822@gmail.com>
2018-04-11 18:08:20 +0900

    change promise-mysql to knex

--- common/modules/db.js
+++ common/modules/db.js
@@ -2,15 +2,15 @@ const mysql = require('promise-mysql')
 const PRIVACY = require('../../privacy.json')
 
 
-const config = {
-    host: PRIVACY.MYSQL.HOST,
-    port: PRIVACY.MYSQL.PORT,
-    user: PRIVACY.MYSQL.USER,
-    password: PRIVACY.MYSQL.PASSWORD,
-    database: PRIVACY.MYSQL.DATABASE,
-    connectionLimit: 100,
-};
+const db = require('knex')({
+    client: 'mysql',
+    connection: {
+        host : PRIVACY.MYSQL.HOST,
+        user : PRIVACY.MYSQL.USER,
+        password : PRIVACY.MYSQL.PASSWORD,
+        database : PRIVACY.MYSQL.DATABASE
+    }
+})
 
-const connection = mysql.createConnection(config)
 
-module.exports = connection;
+module.exports = db;
