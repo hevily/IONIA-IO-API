4bc1df95545479e83a4296c4efe553e4e83f219d
yoojasung <arnold.yoo@findexchain.com>
2018-04-13 14:59:03 +0900

    feat: change market data structure

--- methods/market/marketData.js
+++ methods/market/marketData.js
@@ -5,17 +5,18 @@ async function getMarketData(params) {
     const url = 'https://api.coinmarketcap.com/v1/ticker/'
     const markets = await http.request(url)
 
-    const data = []
+    const data = {}
 
     for(const market of markets) {
-        data.push({
+        const key = market.symbol
+        data[key] = {
             id: market.id,
             name: market.name,
             symbol: market.symbol,
             rank: parseInt(market.rank),
             priceBTC: parseFloat(market.price_btc),
             percentChange24h: parseFloat(market.percent_change_24h)
-        })
+        }
     }
 
     return {
